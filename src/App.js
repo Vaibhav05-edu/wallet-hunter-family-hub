1a381a9f40492bd5d7286d7f9b628be2e12642ba
Vaibhav05-edu <25f1000866@ds.study.iitm.ac.in>
2025-06-27 23:33:30 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import OTPAuth from "./components/OTPAuth";
+import HeadForm from "./components/HeadForm";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [headData, setHeadData] = useState(null);
 
   return (
     <div>
-      {user ? (
-        <h3>Welcome, {user.phoneNumber}</h3>
-      ) : (
-        <OTPAuth onLogin={setUser} />
+      {!user && <OTPAuth onLogin={setUser} />}
+      {user && !headData && (
+        <HeadForm onSubmit={(formData) => {
+          console.log("Head Data Submitted:", formData);
+          setHeadData(formData);
+        }} />
+      )}
+      {headData && (
+        <div>
+          <h3>Head registered successfully!</h3>
+          <pre>{JSON.stringify(headData, null, 2)}</pre>
+        </div>
       )}
     </div>
   );
